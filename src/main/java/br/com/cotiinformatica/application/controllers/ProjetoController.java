4a47a0681d256369f0f4702c8ee465a7a14f6b81
reblreis <rebreis88@gmail.com>
2024-01-03 15:02:22 -0300

    Desenvolvimento de API, validacao de DTOs em API e tratamento de erros

--- src/main/java/br/com/cotiinformatica/application/controllers/ProjetoController.java
+++ src/main/java/br/com/cotiinformatica/application/controllers/ProjetoController.java
@@ -1,5 +1,6 @@
 package br.com.cotiinformatica.application.controllers;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.cotiinformatica.domain.dtos.ProjetoGetDto;
 import br.com.cotiinformatica.domain.dtos.ProjetoPostDto;
 import br.com.cotiinformatica.domain.interfaces.ProjetoService;
 import jakarta.validation.Valid;
@@ -19,15 +21,15 @@ public class ProjetoController {
 
 	@Autowired
 	ProjetoService projetoService;
-	
+
 	@PostMapping
 	public UUID post(@RequestBody @Valid ProjetoPostDto dto) {
 		return projetoService.criar(dto);
 	}
 
 	@GetMapping
-	public void get() {
-		// TODO Implementar o método
+	public List<ProjetoGetDto> get() {
+		return projetoService.consultar();
 	}
 
 }
